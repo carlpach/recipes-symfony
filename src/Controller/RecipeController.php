011e1b30f80d37dc22bb6dabf9d498a1978f8072
carlos <carlos.pacheco@bootcamp-upgrade.com>
2023-07-04 10:19:24 +0200

    get recipes

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class RecipeController extends AbstractController {
+
+    #[Route("/recipe")]
+    public function getRecipe() {
+        $recipe = [
+            "code" => "466hf5",
+            "name" => "Paella",
+            "country" => "Spain",
+            "image" => "https://www.recetasnestle.com.py/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/876038bcd1cf5abcaa28e86d9705eaf6.webp?itok=mayddXYO",
+            "ingredients" => ["rice", "prawns"]
+        ];
+
+        return $this->render("Recipes/getRecipes.html.twig", ["recipe" => $recipe] );
+    }
+
+    #[Route("/recipes")]
+    public function getRecipes() {
+        $listRecipes = [
+            [
+                "code" => "#0001",
+                "name" => "Paella",
+                "country" => "Spain",
+                "image" => "https://www.recetasnestle.com.py/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/876038bcd1cf5abcaa28e86d9705eaf6.webp?itok=mayddXYO",
+                "ingredients" => ["rice", "prawns"]
+            ],
+            [
+                "code" => "#0002",
+                "name" => "Gazpacho",
+                "country" => "Spain",
+                "image" => "https://www.recetasnestle.com.py/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/876038bcd1cf5abcaa28e86d9705eaf6.webp?itok=mayddXYO",
+                "ingredients" => ["tomato", "olive oil"]
+            ],
+            [
+                "code" => "#0003",
+                "name" => "Pad Thai",
+                "country" => "Thailand",
+                "image" => "https://www.recetasnestle.com.py/sites/default/files/styles/recipe_detail_desktop/public/srh_recipes/876038bcd1cf5abcaa28e86d9705eaf6.webp?itok=mayddXYO",
+                "ingredients" => ["rice", "prawns"]
+            ],
+        ];
+
+        return $this->render("Recipes/listRecipes.html.twig", ["listRecipes" => "$listRecipes"] )
+    }
+}
